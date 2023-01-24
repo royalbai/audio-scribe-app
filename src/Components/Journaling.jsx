1e517fa88bf0ffbc54f6dcd283786d02ad4f694b
Royal <royalbai9@gmail.com>
2023-01-24 17:49:06 -0500

    Restyling the journaling page and mic button functionality

--- src/Components/Journaling.jsx
+++ src/Components/Journaling.jsx
@@ -14,7 +14,8 @@ function Journaling() {
     }
 
     const handleSaveJournal = () => {
-        setJournals([...journals, {title: title,text: transcript, date: new Date().toString()}]);
+        const journalTitle = title !== "" ? title : "Untitled";
+        setJournals([...journals, {title: journalTitle, text: transcript, date: new Date().toString()}]);
         setSaved("Your entry was saved ✔")
         resetTranscript();
     }
@@ -30,21 +31,20 @@ function Journaling() {
 
     return (
         <div className="journaling wrapper">
-        <input type="text" placeholder="Type in your title" onChange={(e) => setTitle(e.target.value)} value={title} />
-        <h3>{listening ? <i className="fa-solid fa-microphone-lines"></i> : <i className="fa-solid fa-microphone-lines-slash"></i>}</h3>
-        <button onClick={toggleMic}>{isListening ? "Stop" : "Start"}</button>
-        <button onClick={handleSaveJournal}>Save Journal</button>
-        <p>{transcript}</p>
-        <p>{saved}</p>
-        <ul>
-            {journals.map((journal, index) => (
-            <li key={index}>
-                <h5>{journal.title}</h5>
-                <p>{journal.text}</p>
-                <p>🚀 {journal.date}</p>
-            </li>
-            ))}
-        </ul>
+            <input type="text" placeholder="Type in your title" onChange={(e) => setTitle(e.target.value)} value={title} />
+            <p className="transcript">{transcript}</p>
+            <h3 onClick={toggleMic}>{listening ? <i className="fa-solid fa-microphone-lines"></i> : <i className="fa-solid fa-microphone-lines-slash"></i>}</h3>
+            <button onClick={handleSaveJournal}>Save Journal</button>
+            <p>{saved}</p>
+            <ul>
+                {journals.map((journal, index) => (
+                <li key={index}>
+                    <h5>{journal.title}</h5>
+                    <p>{journal.text}</p>
+                    <p>🚀 {journal.date}</p>
+                </li>
+                ))}
+            </ul>
         </div>
     );
 };
